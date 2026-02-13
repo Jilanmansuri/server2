85b15eb7e1cc1d7155fe5d43405395e10009bce7
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2026-02-13 16:08:20 +0530

    create

--- server3/index.js
+++ server3/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const app = express();
 
 const users = [
@@ -24,16 +23,16 @@ app.get("/users/:test",(req,res)=>{
    res.status(200).json(users);
 });
 
+
 app.get("/users/:id", (req, res) => {
   const userId = Number(req.params.id);
   const user = users.find(u => u.id === userId);
-
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
-
   res.status(200).json(user);
 });
+
 
 app.listen(3001, () => {
   console.log("Server started on port 3000");
