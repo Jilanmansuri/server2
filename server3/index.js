51430eec354a1c5864e04b54562710331dfaedff
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2026-02-16 11:51:53 +0530

    added delete request

--- server3/index.js
+++ server3/index.js
@@ -1,39 +1,83 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json())
 const users = [
   { id: 1, name: "Arjun", role: "student" },
-  { id: 2, name: "Priyesha", role: "mentor" },
-  { id: 3, name: "Devilal", role: "rockstar" },
-  { id: 4, name: "Nitish", role: "mentor" }
+  { id: 2, name: "Priyesha", role: "mentor" }
 ];
 
 app.get("/", (req, res) => {
   res.send("Express server is running");
 });
 
+
 app.get("/users", (req, res) => {
-    console.log("list of users",users);
+  console.log(users)
   res.status(200).json(users);
 });
 
-app.get("/users/:test",(req,res)=>{
-   console.log(req.params);
-   console.log("first code")
-   res.status(200).json(users);
+app.get("/users/:test", (req, res) => {
+  console.log(req.params);
+  console.log("first code")
+  res.status(200).json(users);
 });
 
 
 app.get("/users/:id", (req, res) => {
   const userId = Number(req.params.id);
   const user = users.find(u => u.id === userId);
+
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
+
   res.status(200).json(user);
 });
 
 
+
+
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+    role: req.body.role
+  };
+
+  users.push(newUser);
+
+  res.status(201).json({
+    message: "User created",
+    user: newUser
+  });
+});
+
+
+
+
+
+
+
+
+
+app.delete("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const index = users.findIndex(u => u.id === userId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  users.splice(index, 1);
+
+  res.status(204).end();
+});
+
+
+
+
+
+
 app.listen(3001, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 3001");
 });
