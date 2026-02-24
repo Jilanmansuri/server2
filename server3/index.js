28ebf11ec997a382f753ff7383ef41028b834b66
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2026-02-24 15:26:16 +0530

    edit

--- server3/index.js
+++ server3/index.js
@@ -145,7 +145,6 @@ app.delete("/users/:id", (req, res) => {
 
 app.delete("/user/:name", (req, res) => {
   const userName = req.params.name;
-
   const index = users.findIndex(
     u => u.name.toLowerCase() === userName.toLowerCase()
   );
